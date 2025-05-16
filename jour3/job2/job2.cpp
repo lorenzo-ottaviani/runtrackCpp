8bfa3b18a53043a70b3737dec8bb4f59b06e3025
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-05-16 14:03:15 +0200

    feat: ajoute jour3, job1

--- jour3/job2/job2.cpp
+++ jour3/job2/job2.cpp
@@ -1,3 +1,38 @@
-//
-// Created by uraie on 15/05/2025.
-//
+/*
+* Auteur : Lorenzo OTTAVIANI.
+* Date : 16/05/2025 14h01
+* But du programme :
+*    Supprimer toutes les voyelles d'une chaîne de caractères.
+* Entrée : ∅
+* Sortie : Affiche la chaîne de caractères convertie dans le terminal.
+*/
+
+#include <iostream>
+#include <string>
+
+bool voyelles(char lettre) {
+    if (lettre == 'a' || lettre == 'e' || lettre == 'i' || lettre == 'o' || lettre == 'u' || lettre == 'y'
+        || lettre == 'A' || lettre == 'E' || lettre == 'I' || lettre == 'O' || lettre == 'U' || lettre == 'Y') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    std::string phrase = "vive la plateforme !";
+
+    int i=0;
+    while (i<phrase.length()) {
+        if (voyelles(phrase[i])) {
+            // Supprime la voyelle en décalant tous les caractères suivants d'une position vers la gauche
+            phrase.erase(i, 1);
+        } else {
+            i++;
+        }
+    }
+
+    std::cout << phrase << std::endl;
+
+    return 0;
+}
